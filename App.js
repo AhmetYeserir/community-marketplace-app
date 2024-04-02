082b2756b5c450477308f5064dbc381c52ad1199
Ahmet Yeşeriroğlu <ahmetgs131@gmail.com>
2024-04-02 16:00:56 +0300

    Authentication işlemi gerçekleştirildi.

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './Apps/Screens/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
   return (
+    <ClerkProvider publishableKey='pk_test_cGlja2VkLXVyY2hpbi04NC5jbGVyay5hY2NvdW50cy5kZXYk'>
     <View className="flex-1 bg-white">
       <StatusBar style="auto" />
-      <LoginScreen/> 
+       
+       <SignedIn>
+          <Text className="mt-10">You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+        <LoginScreen/>
+        </SignedOut>
     </View>
+    </ClerkProvider>
   );
 }
 
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,7 +1,30 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import * as WebBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
+import { useOAuth } from '@clerk/clerk-expo';
 
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+    useWarmUpBrowser();
+ 
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+  
   return (
     <View>
         <Image source={require('./../../assets/images/login.jpeg')} 
@@ -10,7 +33,7 @@ export default function LoginScreen() {
         <View className="p-7 bg-white mt-[-20px] rounded-3xl shadow-md">
             <Text className="text-[30px] font-bold">İkinci El Satış Uygulaması</Text>
             <Text className="text-[18px] text-slate-500 mt-6">Kullanmadığınız ya da eskiyen eşyalarınızı satarak hem ihtiyacı olanlara yardımcı olabilir hem de para kazanabilirsiniz</Text>
-            <TouchableOpacity onPress={()=>console.log("SignIn")} className="p-4 bg-blue-500 rounded-full mt-20">
+            <TouchableOpacity onPress={onPress} className="p-4 bg-blue-500 rounded-full mt-20">
                 <Text className="text-white text-center text-[18px]">Hadi Başlayalım</Text>
             </TouchableOpacity>
         </View>
