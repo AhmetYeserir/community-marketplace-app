9dd02bc1ab994dc12d01ad24f749be431d861782
Ahmet Yeşeriroğlu <ahmetgs131@gmail.com>
2024-04-28 16:39:55 +0300

    ImagePicker ile Add Post ekranına resim yükleyebilme eklendi.

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,11 +1,12 @@
-import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity, Image, ToastAndroid } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig';
 import { Formik } from 'formik';
 import {Picker} from '@react-native-picker/picker'
+import * as ImagePicker from 'expo-image-picker';
 import { getFirestore, getDocs ,collection} from "firebase/firestore";
 export default function AddPostScreen() {
-
+  const [image, setImage] = useState(null);
   const db = getFirestore(app);
   const[categoryList,setCategoryList]=useState([]);
   useEffect(()=>{
@@ -22,16 +23,60 @@ export default function AddPostScreen() {
       setCategoryList(categoryList=>[...categoryList,doc.data()])
     })
   }
+  /**
+   * Used to Pick Image from Gallery
+   */
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+
+  const onSubmitMethod=(value)=>{
+    value.image=image;
+    console.log(value)
+  }
   return (
     <View className="p-10">
       <Text className="text-[27px] font-bold">Yeni Post Ekle</Text>
       <Text className="text-[16px] text-gray-500 mb-7">Yeni Post Oluştur ve Satışa Başla</Text>
       <Formik
         initialValues={{title:'',desc:'',category:'',address:'',price:'',image:''}}
-        onSubmit={value=>console.log(value)}
+        onSubmit={value=>onSubmitMethod(value)}
+        validate={(values)=>{
+          const errors={}
+          if(!values.title)
+          {
+            console.log("Başlık Mevcut Değil");
+            ToastAndroid.show('Başlık Gerekli',ToastAndroid.SHORT)
+            errors.name="Başlık Gerekli"
+          }
+          return errors
+        }}
       >
-        {({handleChange,handleBlur,handleSubmit,values, setFieldValue})=>(
-          <View>
+        {({handleChange,handleBlur,handleSubmit,values, setFieldValue,errors})=>(
+         <View>
+
+          <TouchableOpacity onPress={pickImage}>
+          {image?
+          <Image source={{uri:image}} style={{width:100,height:100,borderRadius:15}}  />
+          :
+          <Image source={require('./../../assets/images/placeholder.jpg')}
+          style={{width:100,height:100,borderRadius:15}}
+          />}
+
+            </TouchableOpacity> 
             <TextInput
               style={styles.input}
               placeholder='Başlık'
