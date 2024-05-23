1a9b41101e97471e802c1b98cc9ac5378e09da25
Ahmet Yeşeriroğlu <ahmetgs131@gmail.com>
2024-05-23 21:40:25 +0300

    Veritabanına kaydedilen ürünlerin detay kısımları eklendi. Ürünler detaylandırıldı, geliştirildi.

--- Apps/Navigations/HomeScreeStackNav.jsx
+++ Apps/Navigations/HomeScreeStackNav.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../Screens/HomeScreen';
 import ItemList from '../Screens/ItemList';
+import ProductDetail from '../Screens/ProductDetail';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +22,16 @@ export default function HomeScreeStackNav() {
             backgroundColor:'#3b82f6',
         },
     headerTintColor:'#fff'})}
+        />
+        <Stack.Screen name='product-detail' 
+        component={ProductDetail}
+        options={{
+            headerStyle:{
+                backgroundColor:'#3b82f6',
+            },
+            headerTintColor:'#fff',
+            headerTitle:'Detail'
+        }}
         />
     </Stack.Navigator>
   )
